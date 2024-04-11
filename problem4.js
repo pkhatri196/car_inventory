7a96a18541d67401cd8eab28b161b99d67480167
pkhatri196 <devesh.khatri44@gmail.com>
2024-04-11 20:43:18 +0530

    problem 4 solved

---
+++ problem4.js
@@ -0,0 +1,13 @@
+import inventory from './inventory.js';
+
+export function problem4(inventory){
+   let yearInventory =[];
+
+   for(let i=0;i<inventory.length;i++){
+       yearInventory.push(inventory[i].car_year);
+
+   }
+   return yearInventory;
+
+
+}
